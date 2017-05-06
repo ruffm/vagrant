665512344543bdaca53573677123b46bff169704
ruffm <mvr5567@psu.edu>
2017-05-06 13:23:11 -0400

    Changed apache to lamp stack

---
+++ lamp/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get upgrade -y
+
+apt-get install apache2 -y
+apt-get install curl -y
+apt-get install mysql-server -y # up to you to run mysql_secure_installation
+apt-get install php libapache2-mod-php php-mcrypt php-mysql php-myadmin -y
+if ! [ -L /var/www ]; then
+	rm -rf /var/www
+	ln -fs /vagrant/ /var/www
+fi
