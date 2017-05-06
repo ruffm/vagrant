27c481dd4bb7384d4c0dd838f1737e37b9b489c4
ruffm <mvr5567@psu.edu>
2017-05-06 13:43:32 -0400

    Reworking bootstrap.sh

--- lamp/bootstrap.sh
+++ lamp/bootstrap.sh
@@ -5,8 +5,8 @@ apt-get upgrade -y
 
 apt-get install apache2 -y
 apt-get install curl -y
-apt-get install mysql-server -y # up to you to run mysql_secure_installation
-apt-get install php libapache2-mod-php php-mcrypt php-mysql php-myadmin -y
+# apt-get install mysql-server -y # up to you to run mysql_secure_installation
+# apt-get install php libapache2-mod-php php-mcrypt php-mysql php-myadmin -y
 if ! [ -L /var/www ]; then
 	rm -rf /var/www
 	ln -fs /vagrant/ /var/www
